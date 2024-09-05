f549471d119519c434ccd807bf04b2a116a0d18e
Pontus Grandin <smeaurix@gmail.com>
2024-09-05 17:59:51 +0200

    basic darkmode setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,6 +10,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        lightbg: '#f5f5f5',
+        darkbg: '#020305',
+        primary: '#1d4ed8',
       }
     }
   },
