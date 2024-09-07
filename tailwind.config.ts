c1f76373540d1eafd737b8d7a41860f7533b5684
Pontus Grandin <smeaurix@gmail.com>
2024-09-07 09:48:18 +0200

    working eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,21 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        lightbg: '#f5f5f5',
-        darkbg: '#020305',
-        primary: '#1d4ed8',
-      }
-    }
+        lightbg: "#f5f5f5",
+        darkbg: "#020305",
+        primary: "#1d4ed8",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
 export default config;
