8d240ded0c7a43655939d69b3eb792be33adeebc
Pontus Grandin <smeaurix@gmail.com>
2024-09-19 01:36:29 +0200

    basic jest setup

---
+++ __tests__/page.test.jsx
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/[locale]/page";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+// TODO fix this as getMessages is not supported in client components
+/* beforeAll(async () => {
+  messages = await getMessages();
+}); */
+
+describe("Page", () => {
+  it("renders a heading", () => {
+    render(
+      <NextIntlClientProvider locale="en">
+        <Home />
+      </NextIntlClientProvider>,
+    );
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument();
+  });
+});
+
