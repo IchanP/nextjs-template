b920a3158ebb5df988f2cf984693ce5c067f4a75
Pontus <smeaurix@gmail.com>
2025-07-10 14:21:55 +0200

    eslint setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home(): React.JSX.Element {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="mt-48 w-[200px] h-[200px] bg-primary-blue">
-        <p>Hello, this is using noto-sans</p>
       </div>
     </div>
   );
